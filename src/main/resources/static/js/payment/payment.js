fba79ed4ae6d64e250e47284c3c28d9793e030c9
dev-AustinKim <ehdgks0412@gmail.com>
2023-04-28 10:54:19 +0900

    쿠폰창

--- src/main/resources/static/js/payment/payment.js
+++ src/main/resources/static/js/payment/payment.js
@@ -1,21 +1,48 @@
 /**
- * 
+ *
  */
 
+let check = false;
+$('.Checkbox_icon').on('click', $('.Checkbox_icon'), function () {
+    /*check=$("#checkboxtf").is(":checked");*/
+    console.log(check);
+    if (!check) {
+        $('#realbutton').css('opacity', '1');
+        $('#checkboxmint').css('backgroundColor', '#00c4c4');
+        check = true;
+    } else {
+        $('#realbutton').css('opacity', '0');
+        $('#checkboxmint').css('backgroundColor', '#fff');
+        check = false;
+    }
+});
 
- let check=false;
- $(".Checkbox_icon").on("click", $(".Checkbox_icon"), function(){
-     /*check=$("#checkboxtf").is(":checked");*/
-     console.log(check);
-     if(!check){
-         $("#realbutton").css('opacity','1');
-         $("#checkboxmint").css('backgroundColor', '#00c4c4');
-         check = true;
-     }else{
-         $("#realbutton").css('opacity','0');
-         $("#checkboxmint").css('backgroundColor', '#fff');
-         check = false;
-     }
- });
- 
- 
+/* 쿠폰창 모달 */
+
+const openModalBtn = document.getElementById('open-modal');
+const modal = document.getElementById('coupon-modal');
+const closeModalBtn = document.getElementsByClassName('close')[0];
+
+openModalBtn.onclick = function () {
+    modal.style.display = 'block';
+};
+
+closeModalBtn.onclick = function () {
+    modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+};
+
+const modalForm = document.querySelector('#modal-form');
+
+modalForm.addEventListener('submit', function(event) {
+  // 기본 동작을 막는다
+  event.preventDefault();
+
+  // 폼 데이터를 전송하는 코드
+  // ...
+});
