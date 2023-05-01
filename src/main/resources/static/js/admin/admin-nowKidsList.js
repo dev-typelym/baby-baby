9f0957a154f62834a51cfe4ebc072309260d4da4
dev-francine <test.code0624@gmail.com>
2023-05-01 23:32:05 +0900

    황현수 파비콘 추가

--- src/main/resources/static/js/admin/admin-nowKidsList.js
+++ src/main/resources/static/js/admin/admin-nowKidsList.js
@@ -1,6 +1,5 @@
 
 /* 상세보기 모달 ===================== */
-// 모달 가상요소로 불러오기
 const $showDetail = $(".row");
 
 $showDetail.on('click', function(e){
@@ -26,4 +25,16 @@ $cancelDelete.on('click', function(e){
     $(".delete-modal").hide();
 })
 
+
+// 수정하기 썸네일
+$('.update-profile').change(function(event) {
+    var input = event.target;
+    var reader = new FileReader();
+    reader.onload = function() {
+      var dataURL = reader.result;
+      var image = $('.img-updated');
+      image.attr('src', dataURL);
+    };
+    reader.readAsDataURL(input.files[0]);
+  });
 
