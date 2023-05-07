0127ec4d1cd73ae8d0158561e3e0952023106b58
dev-typelym <dlstk3031@gmail.com>
2023-05-07 10:17:17 +0900

    메인, 부모님마당, 리뷰게시판 퍼블리싱 완료

--- src/main/resources/static/js/review-board/review-board-write.js
+++ src/main/resources/static/js/review-board/review-board-write.js
@@ -56,13 +56,36 @@ $inputTitle.keyup(() => {
     $maxCount.text((40 - $inputTitle.val().length) + "자 남음");
 });
 
-/* 리셋 버튼 */
+/* 별점 리셋 버튼 */
 const $resetBtn = $(".reset-btn")[0];
-const $fieldStudyTitle = $(".normal-text-input")[0];
-const $fieldStudyDate = $(".field-study-date-input")[0];
+const stars = document.querySelectorAll('.review-my-star');
+stars.forEach((star) => {
+    $resetBtn.addEventListener('click', () => {
+        const resetIndex = star.getAttribute('data-index');
 
-$resetBtn.onclick = () => {
-    $fieldStudyTitle.value = '';
-    $fieldStudyDate.value = '';
-};
+        // 클릭된 star 이미지와 그 이전 이미지의 모든 grey-star 이미지를 변경
+        for (let i = 1; i <= 5; i++) {
+            const resetImg = document.querySelector(`.review-my-star[data-index="${i}"]`);
+            resetImg.style.fill = 'rgba(0, 0, 0, 0.5)';
+        }
+    });
+});
+/* 별점 */
 
+stars.forEach((star) => {
+    star.addEventListener('click', () => {
+        const index = star.getAttribute('data-index');
+
+        // 클릭된 star 이미지와 그 이전 이미지의 모든 grey-star 이미지를 변경
+        for (let i = 1; i <= index; i++) {
+            const img = document.querySelector(`.review-my-star[data-index="${i}"]`);
+            img.style.fill = '#00c4c4';
+        }
+
+        // 클릭된 star 이미지 이후의 grey-star 이미지를 변경
+        for (let i = parseInt(index) + 1; i <= 5; i++) {
+            const img = document.querySelector(`.review-my-star[data-index="${i}"]`);
+            img.style.fill =  'rgba(0, 0, 0, 0.5)';
+        }
+    });
+});
