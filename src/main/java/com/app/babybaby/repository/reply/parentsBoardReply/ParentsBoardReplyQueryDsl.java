e4361e647bf274752d6cea5a3bbf2a8f56b1fe7f
dev-AustinKim <ehdgks0412@gmail.com>
2023-05-10 03:57:15 +0900

    nowKids 퍼블리싱 수정

--- src/main/java/com/app/babybaby/repository/reply/parentsBoardReply/ParentsBoardReplyQueryDsl.java
+++ src/main/java/com/app/babybaby/repository/reply/parentsBoardReply/ParentsBoardReplyQueryDsl.java
@@ -1,4 +1,6 @@
 package com.app.babybaby.repository.reply.parentsBoardReply;
 
 public interface ParentsBoardReplyQueryDsl {
+
+    public Long parentsBoardReplyCount();
 }
---
+++ src/main/java/com/app/babybaby/search/board/parentsBoard/ParentsBoardSearch.java
@@ -0,0 +1,9 @@
+package com.app.babybaby.search.board.parentsBoard;
+
+import lombok.Data;
+
+@Data
+public class ParentsBoardSearch {
+    private String parentsBoardTitle;
+    private String parentsBoardContent;
+}
