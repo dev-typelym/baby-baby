8c82a6b80f5bb9fe411eb9a50c37a84fd141adca
dev-AustinKim <ehdgks0412@gmail.com>
2023-05-10 16:31:52 +0900

    gitignore및 테스트 중

--- src/main/java/com/app/babybaby/repository/board/parentsBoard/ParentsBoardQueryDsl.java
+++ src/main/java/com/app/babybaby/repository/board/parentsBoard/ParentsBoardQueryDsl.java
@@ -1,16 +1,23 @@
 package com.app.babybaby.repository.board.parentsBoard;
 
+import com.app.babybaby.entity.board.event.Event;
 import com.app.babybaby.entity.board.parentsBoard.ParentsBoard;
+import com.app.babybaby.search.board.parentsBoard.ParentsBoardSearch;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
 public interface ParentsBoardQueryDsl {
-//    전체 목록
-    public Page<ParentsBoard> findAllWithSearch(Pageable pageable);
+    //    전체 목록
+    public Page<ParentsBoard> findAllWithSearch(ParentsBoardSearch parentsBoardSearch, Pageable pageable);
 
-//    상세보기
+    //    상세보기
     public Optional<ParentsBoard> findById(Long id);
 
+    //    작성하기(1단계)
+    public Optional<Event> findByEventId(Long id);
+
+
+//    작성하기(2단계 대표사진)
 }
--- src/main/java/com/app/babybaby/search/board/parentsBoard/ParentsBoardSearch.java
+++ src/main/java/com/app/babybaby/search/board/parentsBoard/ParentsBoardSearch.java
@@ -1,9 +1,12 @@
 package com.app.babybaby.search.board.parentsBoard;
 
+import com.app.babybaby.type.CategoryType;
 import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Data @Getter
 public class ParentsBoardSearch {
     private String parentsBoardTitle;
     private String parentsBoardContent;
+    private CategoryType categoryType;
 }
