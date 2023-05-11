a9488364957b85731509c235939aafd41614376b
dev-francine <test.code0624@gmail.com>
2023-05-11 13:07:30 +0900

    황현수 멤버 환경 변경중

--- src/main/java/com/app/babybaby/domain/memberDTO/MemberDTO.java
+++ src/main/java/com/app/babybaby/domain/memberDTO/MemberDTO.java
@@ -1,4 +1,46 @@
 package com.app.babybaby.domain.memberDTO;
 
+import com.app.babybaby.entity.alert.Alert;
+import com.app.babybaby.entity.embeddable.Address;
+import com.app.babybaby.entity.purchase.coupon.Coupon;
+import com.app.babybaby.type.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@Data
+@NoArgsConstructor
 public class MemberDTO {
+
+    private Long id;
+    private String  memberEmail;
+    private String memberName;
+    private String memberPassword;
+    private String memberNickname;
+    private String memberHiSentence;
+    private String memberPhone;
+    private Address memberAddress;
+    private String memberProfileOriginalName;
+    private String memberProfileUUID;
+    private String memberProfilePath;
+    @DateTimeFormat(pattern = "yyyy.MM.dd")
+    private LocalDateTime memberRegisterDate;
+    private MemberType memberType;
+    private Role memberRole;
+    private AcceptanceType memberGuideStatus;
+    private SleepType memberSleep;
+    private GuideType memberGuideType;
+    private CategoryType memberGuideInterest;
+    private String memberFilePath;
+    private String memberFileUUID;
+    private String memberFileOriginalName;
+    private List<Alert> alerts;
+    private List<Coupon> coupons;
+
+
 }
--- src/test/java/com/app/babybaby/repository/member/member/MemberRepositoryTests.java
+++ src/test/java/com/app/babybaby/repository/member/member/MemberRepositoryTests.java
@@ -1,12 +1,19 @@
 package com.app.babybaby.repository.member.member;
 
 import com.app.babybaby.entity.alert.Alert;
+import com.app.babybaby.entity.embeddable.Address;
+import com.app.babybaby.entity.member.Member;
+import com.app.babybaby.type.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @SpringBootTest
 @Slf4j
@@ -22,4 +29,46 @@ public class MemberRepositoryTests {
             log.info(member.toString());
         });
     }
+
+    //    일반 회원가입
+//    회원 20명 저장
+    @Test
+    public void save20MemberTest() {
+        Address address = new Address();
+        address.setAddress("분당");
+        address.setAddressDetail("d");
+        address.setAddressSubDetail("dd");
+        address.setPostcode("038316");
+
+        for (int i = 0; i < 20; i++) {
+            String uniqueNickname = "Nickname" + i;
+
+            Member member = new Member(
+                    "you" + i + "@gmail.com",
+                    "이름" + i,
+                    "1234",
+                    uniqueNickname,
+                    "안녕하세요",
+                    "0101234123" + i,
+                    address,
+                    "defaultImage.png",
+                    "defaultImageUUID",
+                    "/defaultImage",
+                    LocalDateTime.now(),
+                    null,
+                    null,
+                    SleepType.AWAKE,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null
+            );
+
+            memberRepository.save(member);
+        }
+
+    }
+
+
 }
