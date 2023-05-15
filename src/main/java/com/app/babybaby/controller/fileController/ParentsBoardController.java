dafe8f545ef13e8eaad0ec9730421c15317746a8
dev-AustinKim <ehdgks0412@gmail.com>
2023-05-15 15:37:37 +0900

    중간 커밋

--- src/main/java/com/app/babybaby/controller/fileController/ParentsBoardController.java
+++ src/main/java/com/app/babybaby/controller/fileController/ParentsBoardController.java
@@ -1,4 +1,13 @@
 package com.app.babybaby.controller.fileController;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/parentsYard/*")
+@RequiredArgsConstructor
 public class ParentsBoardController {
 }
