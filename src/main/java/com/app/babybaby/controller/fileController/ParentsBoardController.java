535859fb37b8cb2a9a30e0fcce066b2d7212155e
dev-AustinKim <ehdgks0412@gmail.com>
2023-05-15 15:37:52 +0900

    Revert "중간 커밋"
    This reverts commit dafe8f545ef13e8eaad0ec9730421c15317746a8.

--- src/main/java/com/app/babybaby/controller/fileController/ParentsBoardController.java
+++ src/main/java/com/app/babybaby/controller/fileController/ParentsBoardController.java
@@ -1,13 +1,4 @@
 package com.app.babybaby.controller.fileController;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@Slf4j
-@RequestMapping("/parentsYard/*")
-@RequiredArgsConstructor
 public class ParentsBoardController {
 }
--- src/main/java/com/app/babybaby/domain/boardDTO/parentsBoardDTO/ParentsBoardDTO.java
+++ src/main/java/com/app/babybaby/domain/boardDTO/parentsBoardDTO/ParentsBoardDTO.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class ParentsBoardDTO {
     private Long id;
     private String eventTitle;
-    private String memberNickname;
+    private String memberNickName;
     private CategoryType eventCategory;
     private String parentsBoardContent;
     private String parentsBoardTitle;
