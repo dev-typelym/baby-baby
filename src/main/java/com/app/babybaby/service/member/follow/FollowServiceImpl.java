4af143335d4c0ccf6617e8ca3206a4115358dbab
dev-AustinKim <ehdgks0412@gmail.com>
2023-05-24 02:31:32 +0900

    ㅁㄴ

--- src/main/java/com/app/babybaby/service/member/follow/FollowServiceImpl.java
+++ src/main/java/com/app/babybaby/service/member/follow/FollowServiceImpl.java
@@ -1,7 +1,11 @@
 package com.app.babybaby.service.member.follow;
 
+import com.app.babybaby.domain.alertDTO.AlertFollowDTO;
+import com.app.babybaby.domain.memberDTO.MemberDTO;
+import com.app.babybaby.entity.alert.alertFollow.AlertFollow;
 import com.app.babybaby.entity.member.Follow;
 import com.app.babybaby.entity.member.Member;
+import com.app.babybaby.repository.alert.alertFollow.AlertFollowRepository;
 import com.app.babybaby.repository.member.follow.FollowRepository;
 import com.app.babybaby.repository.member.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +16,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.servlet.http.HttpSession;
 import javax.swing.text.html.parser.Entity;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,14 +27,26 @@ import javax.swing.text.html.parser.Entity;
 public class FollowServiceImpl implements FollowService {
 
     private final FollowRepository followRepository;
-
+    private final AlertFollowRepository alertFollowRepository;
     private final MemberRepository memberRepository;
+    private final HttpSession session;
+
 
     @Override
     public void saveFollow(Long followerId, Long followingId) {
         Member follower = memberRepository.findById(followerId).get();
         Member following = memberRepository.findById(followingId).get();
         Follow follow1 = new Follow(follower, following);
+//        AlertFollow alertFollow = new AlertFollow(
+//                a
+//        );
+        MemberDTO memberDTO = (MemberDTO) session.getAttribute("member");
+//        현재 로그인한 아이디
+        Long memberId = memberDTO.getId();
+
+//        if(following.getId()==memberId) {
+//            alertFollowRepository.save()
+//        }
         followRepository.save(follow1);
     }
 
